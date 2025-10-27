266dd0d122dd7e255fdaefbc29cf417e1af128ae
is13mael <ismaelparramoussaif@gmail.com>
2025-10-27 19:42:54 +0100

    first commit

---
+++ src/App.java
@@ -0,0 +1,26 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        try{
+        System.out.print("Introduzca un número entero positivo (relativamente grande): ");
+        int Superior = Integer.parseInt(System.console().readLine());
+        
+        System.out.print("Introduzca otro numero (relativamente pequeño): ");
+        int Limite = Integer.parseInt(System.console().readLine());
+System.out.printf("""
+                Los numeros enteros ppositivos menores que %d que no son divisibles
+                entre %d son los siguientes:   
+                        """,Superior,Limite );
+        for(int i = 1; i < Superior; i++){
+            
+                        if (i % Limite !=0) {
+                System.out.print(i+ " ");
+            }
+        }
+        
+    }catch(NumberFormatException e){
+        System.out.println("Número mal introducido.");
+    }catch(Exception e){
+        System.out.println("Ha ocurrido un error inesperado.");
+    }
+}
+}
